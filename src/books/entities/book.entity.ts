86de75ba0e733f04fa6b335518a9805dc0f3312c
ohansck <ohansck@gmail.com>
2024-01-31 14:42:50 +0100

    Define book entity with properties

--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -2,5 +2,21 @@ import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateCol
 
 @Entity('books')
 export class Book {
+    @PrimaryGeneratedColumn()
+    id: number;
 
+    @Column({ length: 255 })
+    title: string;
+
+    @Column({ length: 255 })
+    author: string;
+
+    @Column('text')
+    description: string;
+
+    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)' })
+    dateAdded: Date;
+
+    @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)', onUpdate: 'CURRENT_TIMESTAMP(6)' })
+    dateUpdated: Date;
 }
