1001c2d780596e6c41d892a22a931cd24c6a525b
ohansck <ohansck@gmail.com>
2024-02-01 14:47:00 +0100

    Dependency modification

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { Book } from './books/entities/book.entity';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.HOST,
+      host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
